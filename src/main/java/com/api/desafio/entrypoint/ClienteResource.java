fb5ebfc1a5c923bfa9151554ea51b2e31d1d524b
unknown <christopherpossonato@gmail.com>
2024-01-25 13:10:43 -0300

    Ajustes na Url

--- src/main/java/com/api/desafio/entrypoint/ClienteResource.java
+++ src/main/java/com/api/desafio/entrypoint/ClienteResource.java
@@ -21,7 +21,7 @@ public class ClienteResource {
     @Autowired
     private ModelMapper mapper;
 
-    @PostMapping
+    @PostMapping("/clientes")
     public ResponseEntity<Long> salvarCliente(@RequestBody @Valid ClienteRequest clienteRequest) {
 
         var clienteDto = mapper.map(clienteRequest, ClienteDto.class);
@@ -31,33 +31,33 @@ public class ClienteResource {
         return ResponseEntity.ok().body(clienteId);
     }
 
-    @GetMapping
+    @GetMapping("/clientes")
     public ResponseEntity<List<ClienteDto>> listar() {
         var list = this.clienteService.listar();
 
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/clientes/{id}")
     public ResponseEntity<?> buscarId(@PathVariable Long id) {
         var obj = clienteService.buscarId(id);
 
         return ResponseEntity.ok().body(obj);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/clientes/{id}")
     public void deletar(@PathVariable Long id) {
         clienteService.excluir(id);
     }
 
-    @PutMapping
+    @PutMapping("/clientes")
     public ResponseEntity<Long> atualizar(@RequestBody ClienteDto clienteDto) {
         var clienteAtualizado = clienteService.alterar(clienteDto);
 
        return ResponseEntity.ok().body(clienteAtualizado);
     }
 
-    @GetMapping("/buscarnome/{nome}")
+    @GetMapping("/clientes/buscarnome/{nome}")
     public ResponseEntity<?> buscarNome(@PathVariable String nome) {
         List<ClienteResponse> clientesNome = clienteService.buscarNome(nome);
 
